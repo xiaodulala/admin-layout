41f656fa1faeebb95aec037c139e80437840a572
duyongMac <tyduyong@126.com>
2022-03-04 16:17:31 +0800

    feat(swagger): add swagger

---
+++ api/docs/doc.go
@@ -0,0 +1,27 @@
+// Package docs admin-layout  Server API.
+//
+//     Schemes: http, https
+//     Host: localhost:9099
+//     BasePath: /
+//     Version: 1.0.0
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//     - api_key
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//    api_key:
+//      type: apiKey
+//      name: Authorization
+//      in: header
+//
+// swagger:meta
+package docs
---
+++ cmd/genswaggertypedocs/swagger_type_docs.go
@@ -0,0 +1,9 @@
+package main
+
+//go:generate swagger generate spec -o ../../api/swagger/swagger.yaml --scan-models
+
+import (
+	_ "github.com/xiaodulala/admin-layout/api/docs"
+)
+
+func main() {}
