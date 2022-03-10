8284e33804ee75d55199009f8e89b4f6455a2d35
duyongMac <tyduyong@126.com>
2022-03-10 09:59:05 +0800

    docs(config): update db config

--- cmd/appctl/main.go
+++ cmd/appctl/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	appctl2 "github.com/xiaodulala/admin-layout/internal/appctl"
+	"github.com/xiaodulala/admin-layout/internal/appctl"
 )
 
 func main() {
-	appctl2.Execute()
+	appctl.Execute()
 }
